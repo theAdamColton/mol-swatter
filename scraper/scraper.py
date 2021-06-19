bfdd3e98538f27ee9fe4811829ad80a794bce661
adam <atcolton@tutanota.com>
2021-06-19 01:02:25 -0600

    modified:   README.md 	new file:   requirements.txt 	modified:   scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,76 +5,75 @@ from pathvalidate import sanitize_filename
 import time
 import logging
 import os
+import sys
 
 DATA_DIR = 'raw_data/'
-LOG_DIR = 'logging/'
-URL ="https://webbook.nist.gov/cgi/cbook.cgi?Value=10%2C1&VType=MW&Formula=&AllowOther=on&AllowExtra=on&Units=SI&cIR=on"
+LOGGING_PROGRESS_FILE = 'logging/progress'
 
+# This will let the script save the last max_size for easy resuming
 LOGGING = True
+# Set this to true to make repeat files redownload
 REPEAT_DL = False
 
 def main():
-    startLogger() 
+    logging.getLogger().setLevel(logging.INFO)
+    starting_mass = 1
+    if len(sys.argv) > 1:
+        starting_mass = int(sys.argv[1])
+    else:
+        res = get_progress()
+        starting_mass = res if res is not None else starting_mass
+    url = get_url(starting_mass)
+    logging.info("Scraping from starting mass {} url: {}".format(starting_mass, url))
+    scrape(url, 300) 
 
-    logging.info("scraping from url:"+URL)
-    scrape(URL, 300) 
-    
-def startLogger():
-    if not LOGGING:
+def get_url(starting_mass):
+    url ="https://webbook.nist.gov/cgi/cbook.cgi?Value={}-500&VType=MW&Formula=&AllowOther=on&AllowExtra=on&Units=SI&cIR=on".format(starting_mass)
+    return url
+
+def save_progress(max_size):
+    f = open(LOGGING_PROGRESS_FILE, "w")
+    f.write(max_size)
+    f.close()
+
+def get_progress():
+    try:
+        f = open(LOGGING_PROGRESS_FILE, "r")
+        max_size = float(f.read())
+        f.close()
+        logging.info("Loaded {} from progress file".format(max_size))
+        return max_size
+    except Exception as e:
+        return None
+
+def scrape(url, max_size):
+    try:
+        res = requests.get(url)
+    except Exception as e:
+        logging.warning("failed to get url : " + url + " - error : " + str(e))
+
+    soup = BeautifulSoup(res.content, 'html.parser')
+    list_parent = soup.find("body").find("ol")
+    li_list = list_parent.find_all('li')
+    # Attempts to find the molar mass of the last element in the ol list
+    final_size = li_list[-1]
+    try:
+        final_size = float(final_size.find("strong").text.strip())
+    except Exception as e:
+        logging.warning("failed to find finalSize :" + str(e))
         return
+    logging.info("Final_size set to : " + str(final_size))
+    parseSearchBody(soup, url)
+    # Once this search page has been fully scraped, saves max_size
+    save_progress(final_size)
+    scrape(get_url(final_size), max_size)
 
-    localTime = time.localtime(time.time())
-    logFileName = str(localTime.tm_mon) + "-" + str(localTime.tm_mday) + "-" + str(localTime.tm_year)[2:] + "--" \
-        + str(localTime.tm_hour) + "-" + str(localTime.tm_min) + "-" + str(localTime.tm_sec)
-    logFileName += ".log"
-
-    logFilePath = uniquify(LOG_DIR + logFileName)
-    logging.basicConfig(filename=logFilePath, encoding='utf-8', level=logging.INFO)
-
-
-def scrape(url, maxSize):
-    while True:
-        try:
-            res = requests.get(url)
-        except Exception as e:
-            logging.warning("failed to get url : " + url + " - error : " + str(e))
-            break
-
-        soup = BeautifulSoup(res.content, 'html.parser')
-        
-        listParent = soup.find("body").find("ol")
-        
-        liList = listParent.find_all('li')
-        finalSize = liList[-1]
-
-        try:
-            finalSize = int(float(finalSize.find("strong").text.strip()))
-        except Exception as e:
-            logging.warning("failed to find finalSize :" + str(e))
-            return
-        
-        logging.info("finalSize set to : " + str(finalSize))
-        
-        try:
-            valueIndex = url.rfind("Value")
-            url = url[:valueIndex + 6] + str(finalSize) + "%2C" + str(maxSize) \
-                + "&VType=MW&Formula=&AllowOther=on&AllowExtra=on&Units=SI&cIR=on"
-                                                
-        except Exception as e:
-            logging.warning("failed to adjust url  :" + str())
-            
-        parseSearchBody(soup)
-        
-        logging.info("new search page generated url : " + url)
-
-def parseSearchBody(soup):
-    
+def parseSearchBody(soup, url):
     listParent = soup.find("body").find("ol")
     liList = listParent.find_all('li')
-    
     for li in liList:
         href = li.find("a")["href"]
-        url = urljoin(URL, href)
+        url = urljoin(url, href)
         parseChemPage(url)
 
 def parseChemPage(url):
@@ -84,21 +83,17 @@ def parseChemPage(url):
         logging.warning(e)
         return
     soup = BeautifulSoup(res.content, 'html.parser')
-
     # if the page already has the download jdx link
     if soup.select_one("div[id=jcamp-tabs]") is not None:
         parse_IR_page(url)
         return
-
-    irLink = soup.select_one("a[href*=IR-SPEC]")
-    
-    if irLink is None:
+    # Picks out the link to the IR page
+    ir_link = soup.select_one("a[href*=IR-SPEC]")
+    if ir_link is None:
         logging.warning("no IR-SPEC href found! link:" + url)
         return
-    
-    irLink = urljoin(URL, irLink['href'])
-
-    parse_IR_page(irLink)
+    ir_link = urljoin(url, ir_link['href'])
+    parse_IR_page(ir_link)
 
 def parse_IR_page(url):
     try:
@@ -107,35 +102,25 @@ def parse_IR_page(url):
         logging.warning(e)
         return
     soup = BeautifulSoup(res.content, 'html.parser')
-    
     # for getting the link to the JCAMP download
-    jcampLink = soup.select_one("a[href*=JCAMP]")
-    if jcampLink is None:
+    jcamp_link = soup.select_one("a[href*=JCAMP]")
+    if jcamp_link is None:
         logging.warning("could not find JCAMP href! link:" + url)
         return
-    
-    molFileLink = soup.select_one("a[href*=Str2File]")
-    if molFileLink is not None:
-        molFileLink = urljoin(URL, molFileLink['href'])
-
-    jcampLink = urljoin(URL, jcampLink['href'])
-
+    molfile_link = soup.select_one("a[href*=Str2File]")
+    if molfile_link is not None:
+        molfile_link = urljoin(url, molfile_link['href'])
+    jcamp_link = urljoin(url, jcamp_link['href'])
     # For setting the file name
-    chemName = soup.select_one("h1[id=Top]").text.strip() + ".jdx"
-    
-    jdxName = chemName.format(chemName, sanitize_filename(chemName ))
-    molName = jdxName.removesuffix(".jdx") + ".mol"
-
-
-
+    chemname = soup.select_one("h1[id=Top]").text.strip() + ".jdx"
+    jdxname = chemname.format(chemname, sanitize_filename(chemname ))
+    molname = jdxname.removesuffix(".jdx") + ".mol"
     try:
-        download_file(jcampLink, jdxName)
-
-        if molName is not None:
-            download_file(molFileLink, molName)
+        download_file(jcamp_link, jdxname)
+        if molname is not None:
+            download_file(molfile_link, molname)
     except Exception as e:
         logging.warning(e)
-
 
 def download_file(url, file_name):
     try:
@@ -143,31 +128,24 @@ def download_file(url, file_name):
     except Exception as e:
         logging.warning(e)
         return
-    
     file_path = DATA_DIR + file_name
     new_file_path = uniquify(file_path)
-
     if not REPEAT_DL and file_path != new_file_path:
         logging.info("repeat file:" + file_path)        
         return
-    
     with open(file_path, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
+    print("Downloaded")
     logging.info("Downloaded " + file_path)
-    
 
 def uniquify(path):
     filename, extension = os.path.splitext(path)
     counter = 1
-
     while os.path.exists(path):
         path = filename + " (" + str(counter) + ")" + extension
         counter += 1
-
     return path
-    
-    
 
 main()
